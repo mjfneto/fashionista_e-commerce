8b7e39ab958384cb8ddbd8d108e843037462631f
Mario Freire <huege.design@gmail.com>
2020-05-17 20:24:43 -0300

    chore: First commit; React project with Parcel bundler

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React, { StrictMode } from 'react';
+import { render } from 'react-dom';
+
+render(
+  <StrictMode>
+    <div>App</div>
+  </StrictMode>,
+  document.getElementById('root')
+);
