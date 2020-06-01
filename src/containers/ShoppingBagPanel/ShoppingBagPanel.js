f608e5560ca951b0c3a57d57ce9a8069e2236b72
Mario Freire <huege.design@gmail.com>
2020-06-01 16:53:59 -0300

    refactor: Change spacing the ShoppingBagPanel elements

--- src/containers/ShoppingBagPanel/ShoppingBagPanel.js
+++ src/containers/ShoppingBagPanel/ShoppingBagPanel.js
@@ -15,41 +15,43 @@ const ShoppingBagPanel = ({
 
   return (
     <div className="bag-panel">
-      <nav className="bag-panel__nav">
-        <button onClick={closeSlider} className="bag-panel__button">
-          <ReturnIcon />
-        </button>
-        <h2 className="bag-panel__title">
-          Itens {`(${shoppingBag.bagQuantity})`}
-        </h2>
-      </nav>
-      <ul className="bag-panel__items">
-        {shoppingBag.items.length ? (
-          shoppingBag.items.map((bagItem, idx) => {
-            return (
-              <li className="bag-panel__item" key={`item-${idx}`}>
-                <ShoppingBagItem
-                  bagItem={bagItem}
-                  addItem={addItem}
-                  removeItem={removeItem}
-                  removeAllItems={removeAllItems}
-                />
-              </li>
-            );
-          })
-        ) : (
-          <li className="bag-panel__no-items" id="no-items">
-            <span
-              className="bag-panel__bag-emoji"
-              role="img"
-              aria-label="Duas sacolas de compras coloridas"
-            >
-              🛍️
-            </span>
-            <p>Sua sacola está vazia!</p>
-          </li>
-        )}
-      </ul>
+      <div className="bag-panel__main-wrapper">
+        <nav className="bag-panel__nav">
+          <button onClick={closeSlider} className="bag-panel__button">
+            <ReturnIcon />
+          </button>
+          <h2 className="bag-panel__title">
+            Itens {`(${shoppingBag.bagQuantity})`}
+          </h2>
+        </nav>
+        <ul className="bag-panel__items">
+          {shoppingBag.items.length ? (
+            shoppingBag.items.map((bagItem, idx) => {
+              return (
+                <li className="bag-panel__item" key={`item-${idx}`}>
+                  <ShoppingBagItem
+                    bagItem={bagItem}
+                    addItem={addItem}
+                    removeItem={removeItem}
+                    removeAllItems={removeAllItems}
+                  />
+                </li>
+              );
+            })
+          ) : (
+            <li className="bag-panel__no-items" id="no-items">
+              <span
+                className="bag-panel__bag-emoji"
+                role="img"
+                aria-label="Duas sacolas de compras coloridas"
+              >
+                🛍️
+              </span>
+              <p>Sua sacola está vazia!</p>
+            </li>
+          )}
+        </ul>
+      </div>
       <div className="bag-panel__subtotal-wrapper">
         <p className="bag-panel__subtotal">
           Subtotal - R$ {shoppingBag.subtotal.toFixed(2).replace('.', ',')}
